f846497db154100818b5bd0535a72d7f3ec347f5
Manuel <manuel.mezzanotte@marconirovereto.it>
2026-04-09 02:43:08 +0200

    feat: create entity anime

---
+++ src/main/java/com/example/catalogoanime/model/Anime.java
@@ -0,0 +1,32 @@
+package com.example.catalogoanime.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "anime")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Anime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titolo;
+
+    private String studio;
+
+    private String genere;
+
+    private Integer annoUscita;
+}
