9deff7301c04aeede6c9dbe5fdb09856dab1846b
Manuel <manuel.mezzanotte@marconirovereto.it>
2026-04-09 02:21:31 +0200

    feat: initial setup project spring boot

---
+++ src/test/java/com/example/catalogoanime/CatalogoAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalogoanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoAnimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
