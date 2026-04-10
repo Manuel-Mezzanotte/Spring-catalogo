186aa328529924f1788e599c40a9cfd2312cce28
Manuel <manuel.mezzanotte@marconirovereto.it>
2026-04-10 02:32:59 +0200

    feat: implement custom user

---
+++ src/main/java/com/example/catalogoanime/config/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.catalogoanime.config;
+
+import com.example.catalogoanime.model.AppUser;
+import com.example.catalogoanime.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Utente non trovato"));
+
+        return User.builder()
+                .username(appUser.getUsername())
+                .password(appUser.getPassword())
+                .roles(appUser.getRole())
+                .build();
+    }
+}
---
+++ src/main/java/com/example/catalogoanime/model/AppUser.java
@@ -0,0 +1,23 @@
+package com.example.catalogoanime.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role = "USER";
+}
